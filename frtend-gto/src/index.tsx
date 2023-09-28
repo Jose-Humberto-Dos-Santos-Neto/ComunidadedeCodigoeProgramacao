a3a9967bee4cb9ed2f884474fe7130514ea9da65
Jose <josehumberto27112003@gmail.com>
2023-09-28 13:20:52 -0300

    Primeiro commit - Adicionando arquivos do Front-End

---
+++ frtend-gto/src/index.tsx
@@ -0,0 +1,12 @@
+import * as reactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'assets/css/styles.css'
+import App from './App'
+
+reactDOM.render(
+  <>
+    <App />
+
+  </>,
+  document.getElementById('root')
+)
